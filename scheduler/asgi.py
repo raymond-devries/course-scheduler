26cf77aa57242c91b2b3f280d5e7ea1c3ad63a18
raymond-devries <raymond.l.devries@gmail.com>
2021-07-05 14:29:22 -0700

    Run pre-commit hooks.

--- scheduler/asgi.py
+++ scheduler/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scheduler.settings")
 
 application = get_asgi_application()
